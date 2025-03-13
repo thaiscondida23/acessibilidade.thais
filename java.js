63046a94d015d957630a48b779433b7a46004e57
lula meu amor #13 <141644195+thaiscondida23@users.noreply.github.com>
2025-03-13 08:52:06 -0300

    Update java.js

--- java.js
+++ java.js
@@ -8,3 +8,10 @@ document.addEventListener('DOMContentLoaded', function(){
 
     })
 })
+  diminuiFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+
+    })
+
+})
